32404706407f409858b3ed954d63d44b6e87349f
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-10-07 20:41:35 -0700

    wip

--- api/router.js
+++ api/router.js
@@ -2,9 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+const Recipe = require('./model')
 
-router.use('*', (req, res) => { // eslint-disable-line
-    res.json({ api: 'up' })
+router.get('/:recipe_id', (req, res, next) => {
+    Recipe.getRecipeById(req.params.recipe_id)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(next)
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
